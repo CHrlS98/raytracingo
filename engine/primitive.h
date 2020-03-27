a3e5156b3e085c57d79168062ef1aedae6844994
Charles <charlespoirier2112@gmail.com>
2020-03-27 13:59:45 -0400

    Volumes englobants transformés

--- engine/primitive.h
+++ engine/primitive.h
@@ -20,6 +20,20 @@ enum class PRIMITIVE_TYPE
     SPHERE
 };
 
+struct CubeBox
+{
+    CubeBox();
+    void TransformAndAlign(const sutil::Matrix4x4& model);
+    inline float GetMinX() { return face0[0]; }
+    inline float GetMinY() { return face0[4]; }
+    inline float GetMinZ() { return face0[8]; }
+    inline float GetMaxX() { return face1[3]; }
+    inline float GetMaxY() { return face1[7]; }
+    inline float GetMaxZ() { return face1[11]; }
+    sutil::Matrix4x4 face0;
+    sutil::Matrix4x4 face1;
+};
+
 class Primitive
 {
 public:
@@ -37,7 +51,7 @@ public:
     void Transform(const sutil::Matrix4x4& transform);
 
     // Getter
-    inline OptixAabb GetAabb() const { return m_aabb; }
+    OptixAabb GetAabb() const;
     inline const char* GetIntersectionProgram() const { return m_intersectionProgram.c_str(); }
     inline sutil::Matrix4x4 GetModelMatrix() const { return m_modelMatrix; }
 
@@ -45,10 +59,7 @@ private:
     PRIMITIVE_TYPE m_type;
     sutil::Matrix4x4 m_modelMatrix;
     BasicMaterial m_material;
-    OptixAabb m_aabb;
     std::string m_intersectionProgram;
-
-    void BuildAabb();
 };
 } // namespace host
 } // namespace engine
